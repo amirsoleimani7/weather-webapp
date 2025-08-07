c231fcc005d7bc44c93b95d649ad2735fb8e9732
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2025-08-07 20:18:51 +0330

    api works and connected to the index page

--- weather/main/views.py
+++ weather/main/views.py
@@ -3,67 +3,41 @@ from django.http import HttpResponse
 import requests , json
 
 
-def index(request , user_city_name):
+def index(request):
 
     api_key = "e3c83b511869492240dfe8d76dbfb1ef"
     base_url = "http://api.openweathermap.org/data/2.5/weather"
-    
-    city_name = "tehran"
 
-    complete_url = base_url + "appid=" + api_key + "&=" + city_name
+    city_name = request.GET.get('city_id')
 
+    complete_url = f"{base_url}?q={city_name}&appid={api_key}"
+ 
     response = requests.get(complete_url)
     
-    x = response.json() 
+    x = response.json()
 
     print(f"city is : {city_name}")
-    print(f"response is ; {x}")
+    print(f"the code : {x["cod"]}") # if the cod is 200 means OK
 
+    context =  {}
 
+    context['city'] = city_name
     
-    # if x["cod"] not in [401 , 404]:
-    #     # store the value of "main"
-    #     # key in variable y
-    #     y = x["main"]
+    if city_name :     
+        if x["cod"] not in ['401' , '404']:
+            y = x["main"]
 
-    #     # store the value corresponding
-    #     # to the "temp" key of y
-    #     current_temperature = y["temp"]
+            current_temperature = y["temp"] - 273.15 
+            context['current_temperature'] = round(current_temperature , 3)
+            context['current_pressure'] = y["pressure"]
+            context['current_humidity'] = y["humidity"]
+            z = x["weather"]
+            context['weather_description'] = z[0]["description"]
 
-    #     # store the value corresponding
-    #     # to the "pressure" key of y
-    #     current_pressure = y["pressure"]
-
-    #     # store the value corresponding
-    #     # to the "humidity" key of y
-    #     current_humidity = y["humidity"]
-
-    #     # store the value of "weather"
-    #     # key in variable z
-    #     z = x["weather"]
-
-    #     # store the value corresponding 
-    #     # to the "description" key at 
-    #     # the 0th index of z
-    #     weather_description = z[0]["description"]
-
-    #     print(" Temperature (in kelvin unit) = " +
-    #                     str(current_temperature) + 
-    #         "\n atmospheric pressure (in hPa unit) = " +
-    #                     str(current_pressure) +
-    #         "\n humidity (in percentage) = " +
-    #                     str(current_humidity) +
-    #         "\n description = " +
-    #                     str(weather_description))
-
-    # else : 
-    #     print("city not found !!")
+        else : 
+            print("city not found !!")
     
 
-
-    context = {
-
-    }
-
+    print(f"the context is : {context}")
     return render(request , 'main/index.html' , context)
 
