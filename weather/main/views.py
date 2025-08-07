d2dcb06618b13f413384bb862c3ee0fc2af06055
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2025-08-07 15:45:03 +0330

    main page integrated

--- weather/main/views.py
+++ weather/main/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
-import requests
+from django.http import HttpResponse
+
+
 
 def index(request):
     
+
+
+
+
+    context = {
+
+    }
+
+    return render(request , 'main/index.html' , context)
+
